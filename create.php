f2da10452a304b87a34d43612629b8e6a14671c2
eal172 <eal8hs@virginia.edu>
2021-03-14 14:49:13 -0400

    updating form on main html

--- create.php
+++ create.php
@@ -32,10 +32,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-  <link href="flashy-css.css" rel="stylesheet" type="text/css"/>
+  <link href="style/flashy-css.css" rel="stylesheet" type="text/css"/>
   </head>
 <body>
-
+<!-- Beginning of nav bar (will be replaced with php include for next assignment)-->
       <nav class="navbar navbar-expand-md navbar-dark">
         <a class="navbar-brand" href="main.html">Flashy</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -67,7 +67,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
-
+<!--End of nav bar-->
    <h1 align="center">
      New Flashcard Deck
    </h1>
