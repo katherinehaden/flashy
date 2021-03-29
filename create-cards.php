e65b34096e23313ad78e92db9a7c4487aa797347
katherinehaden <60191248+katherinehaden@users.noreply.github.com>
2021-03-29 19:30:53 -0400

    convert create to php

---
+++ create-cards.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="author" content="Katherine Johnson">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"><!-- got style sheet from example from class -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="style/flashy-css.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<!-- Beginning of nav bar (will be replaced with php include for next assignment)-->
+<nav class="navbar navbar-expand-md navbar-dark">
+    <a class="navbar-brand" href="main.html">Flashy</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+        <ul class="navbar-nav w-100"><!-- looked up bootstrap width to rearrange navbar elements-->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Decks</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="create.html">Create</a>
+                    <a class="dropdown-item" href="study.html">Study</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Games</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="#">Matching Mania</a>
+                    <a class="dropdown-item" href="#">Jeopardy</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- got scripts from example navbar from class -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<!--End of nav bar-->
+
+<?php session_start(); ?>
+
+<div class="container">
+    <h1>Let's add cards to <?php echo $_SESSION['new-deck-name'] ?>! </h1>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+        Front of Card: <input type="text" name="front" class="form-control" autofocus required /> <br/>
+        Back of Card: <input type="text" name="back" class="form-control" required /> <br/>
+        <input type="submit" name = "card-done" value="Add next card" class="btn btn-light" />
+        <input type="submit" name = "deck-done" value="That's the last one!" class="btn btn-light" />
+    </form>
+</div>
+
+<?php
+
+    if(isset($_POST['card-done']))
+    {
+        if(!isset($_SESSION['new-deck-cards']))
+        {
+            $_SESSION['new-deck-cards'] = array(); //an array to hold the new cards
+        }
+        $_SESSION['new-deck-cards'][$_POST['front']] = $_POST['back'];
+        header('Location: create-cards.php');
+    }
+    else if(isset($_POST['deck-done']))
+    {
+        if(!isset($_SESSION['new-deck-cards']))
+        {
+            $_SESSION['new-deck-cards'] = array(); //an array to hold the new cards
+        }
+        $_SESSION['new-deck-cards'][$_POST['front']] = $_POST['back'];
+
+        //put the deck in the database somehow!!!
+        header('Location: deck-created.php');
+    }
+
+?>
+
+</body>
+
+
+<br/>
+So far, $_SESSION(<?php echo session_id() ?>) contains... <br/>
+<?php
+if(isset($_SESSION['cards']))
+{
+    foreach($_SESSION['cards'] as $k => $v)
+    {
+        echo "$k : $v <br/>";
+    }
+}
+?>
+<br/>
+
+
+</html>
---
+++ deck-created.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="author" content="Katherine Johnson">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"><!-- got style sheet from example from class -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="style/flashy-css.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<!-- Beginning of nav bar (will be replaced with php include for next assignment)-->
+<nav class="navbar navbar-expand-md navbar-dark">
+    <a class="navbar-brand" href="main.html">Flashy</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+        <ul class="navbar-nav w-100"><!-- looked up bootstrap width to rearrange navbar elements-->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Decks</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="create.html">Create</a>
+                    <a class="dropdown-item" href="study.html">Study</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Games</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="#">Matching Mania</a>
+                    <a class="dropdown-item" href="#">Jeopardy</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- got scripts from example navbar from class -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<!--End of nav bar-->
+
+<?php session_start(); ?>
+
+<div class="container">
+    <h1> Your new deck has been added! </h1>
+    <?php
+        echo "<h2>" . $_SESSION['new-deck-name'] . "</h2>";
+        // referenced... https://stackoverflow.com/questions/14047296/display-array-as-a-table
+        echo "<table style='font-size:1.5rem;' border='1'>";
+        echo "<tr><th> Front </th> <th> Back </th> </tr>";
+        foreach ($_SESSION['new-deck-cards'] as $front => $back) {
+            echo "<tr><td>" . $front . "</td>" . "<td>" . $back . "</td></tr>";
+
+        }
+        ?>
+
+</div>
+
+
+</body>
+
+<?php
+if(count($_SESSION) > 0)
+{
+    unset($_SESSION['new-deck-name']);
+    unset($_SESSION['new-deck-cards']);
+}
+?>
+
+</html>
