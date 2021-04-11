76ce90050d9dd3ea9bcdf2c23d199c2b85edf76b
katherinehaden <60191248+katherinehaden@users.noreply.github.com>
2021-04-11 12:41:43 -0400

    Fix study page header

--- study.php
+++ study.php
@@ -4,8 +4,13 @@
 <html lang="en">
   <head>
       <meta name="author" content="Katherine Johnson">
-      <link href="style/study-css.css" rel="stylesheet" type="text/css"/>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
+      <meta name="viewport" content="width=device-width, initial-scale=2"> <!-- got style sheet from example from class -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
       <link href="style/flashy-css.css" rel="stylesheet" type="text/css"/>
+      <link href="style/study-css.css" rel="stylesheet" type="text/css"/>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
   </head>
 
 <?php include('header.html')?>
