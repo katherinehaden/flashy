105a0aef58c3c8d89def51012f3e4599f6092419
katherinehaden <60191248+katherinehaden@users.noreply.github.com>
2021-04-11 12:45:13 -0400

    include header file in create files

--- study-main.php
+++ study-main.php
@@ -13,36 +13,7 @@
 </head>
 
 <body>
-<!-- Beginning of nav bar (will be replaced with php include for next assignment)-->
-<nav class="navbar navbar-expand-md navbar-dark">
-    <a class="navbar-brand" href="main.html">Flashy</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
-        <ul class="navbar-nav w-100"><!-- looked up bootstrap width to rearrange navbar elements-->
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Decks</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="create-deck.php">Create</a>
-                    <a class="dropdown-item" href="study-main.php">Study</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Games</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="#">Matching Mania</a>
-                    <a class="dropdown-item" href="#">Jeopardy</a>
-                </div>
-            </li>
-        </ul>
-    </div>
-</nav>
-<!-- got scripts from example navbar from class -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-<!--End of nav bar-->
-
+<?php include('header.html'); ?>
 <?php session_start(); ?>
 
 <div class="container">
