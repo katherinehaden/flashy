e0576d2c5b19826618ad8c256b51ac3f31ae8647
eal172 <eal8hs@virginia.edu>
2021-05-03 19:01:55 -0400

    Adding authors

--- ng-post.php
+++ ng-post.php
@@ -1,5 +1,6 @@
 
 <?php
+//Elena Lensink
 //header('Access-Control-Allow-Origin: http://localhost:4200');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
