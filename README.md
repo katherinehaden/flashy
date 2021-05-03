ee00d44bd00254c45c5d2556879126b70b4a6aa8
eal172 <eal8hs@virginia.edu>
2021-05-03 17:30:40 -0400

    Did not actually commit last time

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ APIs/Resources used:
 * https://www.javascripttutorial.net/javascript-anonymous-functions/
 * https://bootstrapshuffle.com/classes/sizing/w-100
 * https://www.tutsmake.com/php-functions-remove-last-character-from-string-in-php/
-
+* https://www.techiediaries.com/angular/php-angular-9-crud-api-httpclient/
+*https://www.tutorialspoint.com/How-to-Create-a-Multi-line-Text-Input-Text-Area-In-HTML#:~:text=To%20create%20a%20multi%2Dline%20text%20input%2C%20use%20the%20HTML,input%20text%20over%20multiple%20rows.&text=Specifies%20that%20on%20page%20load%20the%20text%20area%20should%20automatically%20get%20focus.
+*https://www.w3schools.com/howto/howto_css_star_rating.asp
 Things to maybe add:
 * update mark for review changes in the database as users study
---
+++ ng-post.php
@@ -0,0 +1,38 @@
+
+<?php
+//header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+
+$postdata = file_get_contents("php://input");
+
+
+$request = json_decode($postdata);
+
+$data =[];
+$sqlentry = [];
+$count = 0;
+foreach($request as $k => $v)
+{
+    $temp = "$k => $v";
+    $data[0]['post_'.$k] = $v;
+    $sqlentry[$count] = $v;
+    $count++;
+}
+
+$current_date = date("Y-m-d");
+
+echo json_encode(['content'=>$sqlentry, 'response_on'=>$current_date]);
+require('connect-db.php');
+    $query = "INSERT INTO feedback (rating, feedback) VALUES (:rating, :feedback)";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':rating', $sqlentry[0]);
+    $statement->bindValue(':feedback', $sqlentry[1]);
+    $statement->execute();
+
+    $statement->closeCursor();
+
+?>
